ef7353bbde70f5c3e728f4c79b3058649fd857ab
jordanyio <jordanjyx.io@gmail.com>
2025-02-20 22:04:16 -0500

    SPA

--- kristina-lalova-portfolio/src/App.js
+++ kristina-lalova-portfolio/src/App.js
@@ -2,24 +2,13 @@
 // src/App.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import ResearchPage from './pages/ResearchPage';
-import ExperiencePage from './pages/ExperiencePage';
-import AchievementsPage from './pages/AchievementsPage';
-import ContactPage from './pages/ContactPage';
-import Layout from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
+      <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/research" element={<ResearchPage />} />
-          <Route path="/experience" element={<ExperiencePage />} />
-          <Route path="/achievements" element={<AchievementsPage />} />
-          <Route path="/contact" element={<ContactPage />} />
         </Routes>
-      </Layout>
     </BrowserRouter>
   );
 }
